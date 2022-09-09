9626dd6fafabb6a3b871cfb36c63577d67746c80
Mitchell Stowman <92927307+StowCode@users.noreply.github.com>
2022-09-08 22:47:07 -0400

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -6,6 +6,9 @@ import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js'
+
+const gui = new GUI()
 
 /////////////////////////////////////////////////////////////////////////
 //// LOADING MANAGER
@@ -170,7 +173,7 @@ scene.add( sunsetLight );
 
             ///////////////// TWEEN LOOP VIA CHAINS
             let target0 = new THREE.Vector3(-2,10,0);
-            let target1 = new THREE.Vector3(-2.5,10,1);
+            let target1 = new THREE.Vector3(-1,10,-1);
             
             let t1 = new TWEEN.Tween(cloud.position) 
                 .to(target1, 5000)
@@ -216,15 +219,30 @@ scene.add( sunsetLight );
         })
 
         loader.load('models/gltf/cloud.glb', (cloud3) => {
-            cloud3.scene.scale.set(.5,.5,.5)
+            cloud3 = cloud3.scene
+            cloud3.scale.set(.5,.5,.5)
 
-            cloud3.scene.position.x = -3
-            cloud3.scene.position.y = 8
-            cloud3.scene.position.z = 8
+            cloud3.position.x = -3
+            cloud3.position.y = 8
+            cloud3.position.z = 8
 
-            cloud3.scene.rotateY(120)
+            let target0 = new THREE.Vector3(-3,8,8);
+            let target1 = new THREE.Vector3(-2.5,8,7);
+            
+            let t1 = new TWEEN.Tween(cloud3.position) 
+                .to(target1, 4000)
+                .easing(TWEEN.Easing.Sinusoidal.InOut)
 
-            scene.add(cloud3.scene)
+            let t2 = new TWEEN.Tween(cloud3.position)
+                .to(target0, 3000)
+                .easing(TWEEN.Easing.Sinusoidal.InOut)
+
+            t1.chain(t2)
+            t2.chain(t1)
+            t1.start();
+
+            cloud3.rotateY(120)
+            scene.add(cloud3)
         })
 
 
@@ -233,7 +251,7 @@ scene.add( sunsetLight );
 function introAnimation() {
     controls.enabled = false //disable orbit controls to animate the camera
     
-    new TWEEN.Tween(camera.position.set(26,4,-35 )).to({ // from camera position
+    new TWEEN.Tween(camera.position.set(26,8,-35 )).to({ // from camera position
         x: 25, //desired x position to go
         y: 20, //desired y position to go
         z: 20 //desired z position to go
@@ -246,7 +264,7 @@ function introAnimation() {
     })
 }
 
-// introAnimation() // call intro animation on start
+introAnimation() // call intro animation on start
 
 /////////////////////////////////////////////////////////////////////////
 //// DEFINE ORBIT CONTROLS LIMITS
@@ -259,6 +277,40 @@ function setOrbitControlsLimits(){
     controls.enableZoom = true
     controls.maxPolarAngle = Math.PI /2.5
 }
+
+// create parameters for GUI
+var params = {color: sunLight.color.getHex(), color2: ambient.color.getHex(), color3: scene.background.getHex()}
+
+
+// create a function to be called by GUI
+const update = function () {
+	var colorObj = new THREE.Color( params.color )
+	var colorObj2 = new THREE.Color( params.color2 )
+	var colorObj3 = new THREE.Color( params.color3 )
+	sunLight.color.set(colorObj)
+	ambient.color.set(colorObj2)
+	scene.background.set(colorObj3)
+}
+
+//////////////////////////////////////////////////
+//// GUI CONFIG
+gui.add(sunLight, 'intensity').min(0).max(2).step(0.0001).name('Sun Intensity')
+gui.add(sunLight.position, 'x').min(-100).max(100).step(0.00001).name('Sun Position X')
+gui.add(sunLight.position, 'y').min(0).max(100).step(0.00001).name('Sun Position Y')
+gui.add(sunLight.position, 'z').min(-100).max(100).step(0.00001).name('Sun Position Z')
+// gui.addColor(params,'color').name('Light Color').onChange(update)
+gui.addColor(params,'color2').name('Ambient color').onChange(update)
+// gui.add(ambient, 'intensity').min(0).max(10).step(0.001).name('Ambient intensity')
+gui.addColor(params,'color3').name('Sky Color').onChange(update)
+
+//////////////////////////////////////////////////
+//// ON MOUSE MOVE TO GET CAMERA POSITION
+document.addEventListener('mousemove', (event) => {
+    event.preventDefault()
+
+    console.log(camera.position)
+
+}, false)
 
 
 const clock = new THREE.Clock()
